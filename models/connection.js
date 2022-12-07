04cf34ea18de52c9d4247c9e40135860716a556e
Vincent Fuertez <vfuertez@gmail.com>
2022-12-07 00:41:45 -0500

    mongo connected

---
+++ models/connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+mongoose.connect(DATABASE_URL, CONFIG)
+
+mongoose.connection
+.on('open', () => console.log('Mongoose Connected'))
+.on('close', () => console.log('Disconnected from Mongo'))
+.on('error', (error) => console.log('Mongoose error',error))
+
+module.exports = mongoose
--- server.js
+++ server.js
@@ -2,3 +2,15 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const methodOverride = require('method-override')
+const PORT = process.env.PORT || 3000
+
+
+
+
+
+
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`connected to port ${PORT}`)
+})
