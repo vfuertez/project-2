64e6da8e95b6cdcb459a12d6823892d0e0c30e74
Vincent Fuertez <vfuertez@gmail.com>
2022-12-07 00:52:21 -0500

    middelware added

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const methodOverride = require('method-override')
+const morgan = require('morgan')
 const PORT = process.env.PORT || 3000
 
+// Middleware
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+app.use(express.static('public'))
 
 
+//Routes
+app.get("/", (req, res) => {
+    res.send('Server is working')
+})
 
 
-
-
-
+// Server Listener
 app.listen(PORT, (req, res) => {
     console.log(`connected to port ${PORT}`)
 })
